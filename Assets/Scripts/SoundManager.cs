a1a4ace28c1f437d36b3b2b18c30dbb82016ca4f
thnphu <thnphu1@gmail.com>
2025-02-27 21:28:21 +1100

    fix some bugs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,64 +2,73 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance; // Singleton
+    public static SoundManager instance; // Singleton instance
 
     public AudioSource walkSound;
     public AudioSource jumpSound;
     public AudioSource pickupSound;
     public AudioSource deathSound;
     public AudioSource stompSound;
-    public AudioSource stageClearSound;
 
     void Awake()
     {
+        // Ensure there's only one instance of SoundManager
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject); // Keep SoundManager across scenes
         }
         else
         {
-            Destroy(gameObject);
-            return;
+            Destroy(gameObject); // Prevent duplicates
         }
-
-        DontDestroyOnLoad(gameObject); // Keeps SoundManager across scenes
     }
 
     public void PlayWalkSound()
     {
         if (!walkSound.isPlaying)
+        {
             walkSound.Play();
+        }
     }
 
     public void StopWalkSound()
     {
         if (walkSound.isPlaying)
+        {
             walkSound.Stop();
+        }
     }
 
     public void PlayJumpSound()
     {
-        jumpSound.Play();
+        if (jumpSound != null)
+        {
+            jumpSound.Play();
+        }
     }
 
     public void PlayPickupSound()
     {
-        pickupSound.Play();
+        if (pickupSound != null)
+        {
+            pickupSound.Play();
+        }
     }
 
     public void PlayDeathSound()
     {
-        deathSound.Play();
+        if (deathSound != null)
+        {
+            deathSound.Play();
+        }
     }
 
     public void PlayStompSound()
     {
-        stompSound.Play();
-    }
-
-    public void PlayStageClearSound()
-    {
-        stageClearSound.Play();
+        if (stompSound != null)
+        {
+            stompSound.Play();
+        }
     }
 }
