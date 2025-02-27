81ea81f3fff0e5fbcf5920ca8fce9852a8b61b94
thnphu <thnphu1@gmail.com>
2025-02-27 20:57:36 +1100

    change to SoundManager script to controll all sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance; // Singleton
+
+    public AudioSource walkSound;
+    public AudioSource jumpSound;
+    public AudioSource pickupSound;
+    public AudioSource deathSound;
+    public AudioSource stompSound;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject); // Keeps SoundManager across scenes
+    }
+
+    public void PlayWalkSound()
+    {
+        if (!walkSound.isPlaying)
+            walkSound.Play();
+    }
+
+    public void StopWalkSound()
+    {
+        if (walkSound.isPlaying)
+            walkSound.Stop();
+    }
+
+    public void PlayJumpSound()
+    {
+        jumpSound.Play();
+    }
+
+    public void PlayPickupSound()
+    {
+        pickupSound.Play();
+    }
+
+    public void PlayDeathSound()
+    {
+        deathSound.Play();
+    }
+
+    public void PlayStompSound()
+    {
+        stompSound.Play();
+    }
+}
